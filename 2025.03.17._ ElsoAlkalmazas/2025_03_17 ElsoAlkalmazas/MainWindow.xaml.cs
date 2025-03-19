76a64fcb36a35a555cea192ca01d58dd31f12b50
detariviktoria <detari.viktoria@ckik.hu>
2025-03-19 11:29:55 +0100

    elsoalkalmazas

---
+++ 2025.03.17._ ElsoAlkalmazas/2025_03_17 ElsoAlkalmazas/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2025_03_17_ElsoAlkalmazas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            gomb.BackGround = new SolidColorBrush(Properties.Settings.Default.MyColor);
+
+
+        }
+    }
+}
